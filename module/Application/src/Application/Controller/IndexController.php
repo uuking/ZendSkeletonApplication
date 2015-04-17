d81f379a4f99c6de755e0ad2e6182c0de05b1a63
uuking <1210889580@qq.com>
2015-04-17 14:10:54 +0800

    Update IndexController.php

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -7,7 +7,7 @@
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
-namespace Application\Controller;
+namespace Application\Controller;yyyy
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
